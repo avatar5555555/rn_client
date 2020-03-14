953a78dadd68f6fc972ded4c943689731031fdda
Aleksey Sergeev <avatar5555555@gmail.com>
2020-03-14 17:56:52 +0400

    feat: add packages
     - styled-components
     - lotty

--- app.tsx
+++ app.tsx
@@ -1,37 +1,19 @@
-import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import React, { Fragment } from "react";
+import { StatusBar } from "react-native";
+import LottieView from "lottie-react-native";
+import styled from "styled-components/native";
+
+const View = styled.View`
+  flex-basis: 100%;
+`;
 
 export const App = () => {
   return (
-    <React.Fragment>
+    <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <View style={styles.body}>
-            <Text>test</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </React.Fragment>
+      <View>
+        <LottieView autoPlay source={require("./loader.json")} />
+      </View>
+    </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-});
