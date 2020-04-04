2e94150d925b258749f8576afb6f86af64bc36f0
Aleksey Sergeev <avatar5555555@gmail.com>
2020-04-04 21:12:23 +0400

    feat: implement sign up logic

---
+++ src/shared-schema.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from "react-intl"
+
+export const sharedErrors = defineMessages({
+  required: {
+    id: "sharedErrors.required",
+    defaultMessage: "Required",
+  },
+  invalid: {
+    id: "sharedErrors.invalid",
+    defaultMessage: "Invalid",
+  },
+  errorTitle: {
+    id: "sharedErrors.errorTitle",
+    defaultMessage: "Oops!",
+  },
+})
